e3fe2c2f5828f9e61f418e8453f6e47282c83cce
vishuhanda <itsvishuhanda@gmail.com>
2023-09-17 11:26:30 +0530

    Added bootstrap in form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
-function App() {
+function Appl() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+      <Form>
+          <Form.Group className='mb-3' controlId='basicEmail'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type='email' placeholder='Enter Email' />
+              <Form.Text className='text-muted'>
+                  we will never share email with you
+              </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+              Submit
+          </Button>
+          {/*<Button className={}></Button>*/}
+      </Form>
+    </div>
     </div>
   );
 }
 
-export default App;
+export default Appl;
