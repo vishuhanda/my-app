aaca53b1d1c929f8479c7d708543a5b117d445c4
vishuhanda <itsvishuhanda@gmail.com>
2023-09-18 10:46:37 +0530

    added changed with router using bootstrap

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,23 +1,26 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link,Outlet} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 
 export default function Header(){
     return(
         <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/home" >Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/blogs" >Contact</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact" >Blogs</Link>
-                        </li>
-                    </ul>
-                </nav>
-
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#home">Vishu Handa</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/home" >Home</Nav.Link>
+                            <Nav.Link href="/contact">Contact</Nav.Link>
+                            <Nav.Link href="/blogs">Blogs</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <Outlet />
         </div>
     )
